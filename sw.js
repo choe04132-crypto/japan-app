3e84d561cbca7d8a2ad5a067df469f04e1184937
cds04 <choe04132@gmail.com>
2026-05-17 21:49:10 +0900

    단어장: 탭 없이 단어+예문 바로 보이게 개편

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'japan-app-v3';
+const CACHE = 'japan-app-v4';
 const ASSETS = ['./', './index.html', './manifest.json', './icon-192.png', './icon-512.png'];
 
 self.addEventListener('install', e => {
